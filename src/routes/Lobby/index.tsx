ee53c9c7f6e06107d1958694180e87b0f78709b4
Gustavo Brandão <gugabgta3@gmail.com>
2023-07-01 10:32:59 -0300

    sick lobby

--- src/routes/Lobby/index.tsx
+++ src/routes/Lobby/index.tsx
@@ -6,8 +6,20 @@ export interface Props {
 
 const Lobby = ({name}: Props) => {
     return (
-        <div>
-            { name ?? 'Lobby' }
+        <div className="Lobby">
+            <h1>Lobby</h1>
+            <div className="input-group">
+                <div className="row">
+                    <label className="player-name-label" htmlFor="player-name">Player Name:</label>
+                    <input type="text" name="player-name" className="player-name" />
+                </div>
+                <div className="row">
+                    <label htmlFor="server-url">Server:</label>
+                    <input type="text" name="server-url" className="server-url" />
+                    <button className="connect">Connect</button>
+                </div>
+            </div>
+            <textarea className="players-lobby" readOnly>opa, suave meu bom?</textarea>
         </div>
     )
 }
