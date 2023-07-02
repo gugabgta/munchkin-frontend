e50ea4de7795752eb0ed2d249c980a658ad46851
Gustavo Brandão <gugabgta3@gmail.com>
2023-07-02 09:22:29 -0300

    lobby connection and logic kind of

--- src/main.tsx
+++ src/main.tsx
@@ -2,33 +2,52 @@ import * as React from "react"
 import * as ReactDOM from "react-dom/client"
 import * as Pages from './routes/Exports'
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
-// import "./main.scss"
+import { io } from "socket.io-client";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Pages.Menu />,
-        errorElement: <Pages.ErrorPage />,
-    },
-    {
-        path: "/game",
-        element: <Pages.Game />,
-        errorElement: <Pages.ErrorPage />,
-    },
-    {
-        path: "/lobby",
-        element: <Pages.Lobby />,
-        errorElement: <Pages.ErrorPage />,
-    },
-    {
-        path: "/debug",
-        element: <Pages.Debug />,
-        errorElement: <Pages.ErrorPage />,
-    },
-])
+// const socket = io('http://localhost:3000');
+function App() {
+    const [socket, setSocket] = React.useState(0)
+
+
+    // socket.on("connect", () => {
+        // console.log(`connected to ${socket}`)
+        // const name = window.prompt('Enter your name')
+        // socket.emit("join", name ?? "Anonymous", res => {
+        //     received.innerHTML += `<p>${res.id}</p>`
+        // })
+    // });
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <Pages.Menu />,
+            errorElement: <Pages.ErrorPage />,
+        },
+        {
+            path: "/game",
+            element: <Pages.Game />,
+            errorElement: <Pages.ErrorPage />,
+        },
+        {
+            path: "/lobby",
+            element: <Pages.Lobby connect={ setSocket } socket={ socket }/>,
+            errorElement: <Pages.ErrorPage />,
+        },
+        {
+            path: "/debug",
+            element: <Pages.Debug />,
+            errorElement: <Pages.ErrorPage />,
+        },
+    ])
+
+    return (
+        <RouterProvider router={router} />
+    )
+}
+
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <App />
     </React.StrictMode>
 )
--- src/routes/Lobby/index.tsx
+++ src/routes/Lobby/index.tsx
@@ -1,25 +1,60 @@
+import { Dispatch } from 'react';
 import './Lobby.scss'
+import { io } from "socket.io-client";
 
 export interface Props {
-    name?: string;
+    connect: any,
+    socket: any,
 }
 
-const Lobby = ({name}: Props) => {
+const Lobby = (props: Props) => {
+    function connectToServer() {
+        const server_input = (document.querySelector('#server-url')! as HTMLInputElement).value
+        const player_name = (document.querySelector('#player-name')! as HTMLInputElement).value
+        if (server_input === '') {
+            return
+        }
+        try {
+            props.connect(io(server_input))
+            setTimeout(() => {}, 1000)
+            props.socket.emit("join", props.socket.id, player_name)
+            listen()
+        } catch (error) {
+            console.error(`connection failed ${error}`)
+        }
+    }
+
+    function listen() {
+        const textarea = document.querySelector('.players-lobby')! as HTMLTextAreaElement
+        try {
+            props.socket.on("connect", () => {
+                console.log(`connected to ${props.socket.id}`)
+            })
+            props.socket.on("lobby_state", (player_list: Array<string>) => {
+                console.log('received lobby_state')
+                textarea.value = ''
+                player_list.forEach(player => {
+                    textarea.value += `${player}\n`
+                })
+            });
+        } catch (error) {}
+    }
+
     return (
         <div className="Lobby">
             <h1>Lobby</h1>
             <div className="input-group">
                 <div className="row">
                     <label className="player-name-label" htmlFor="player-name">Player Name:</label>
-                    <input type="text" name="player-name" className="player-name" />
+                    <input type="text" name="player-name" id="player-name" className="player-name" defaultValue="Gustavo"/>
                 </div>
                 <div className="row">
                     <label htmlFor="server-url">Server:</label>
-                    <input type="text" name="server-url" className="server-url" />
-                    <button className="connect">Connect</button>
+                    <input type="text" name="server-url" id="server-url" className="server-url" defaultValue="http://localhost:3000"/>
+                    <button className="connect" onClick={ connectToServer }>Connect</button>
                 </div>
             </div>
-            <textarea className="players-lobby" readOnly>opa, suave meu bom?</textarea>
+            <textarea className="players-lobby" defaultValue="opa, suave meu bom?" readOnly></textarea>
         </div>
     )
 }
